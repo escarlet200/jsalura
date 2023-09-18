867f6a6218a612fa097d1361d22f4e077631a72b
escarlet200 <114157080+escarlet200@users.noreply.github.com>
2023-09-18 12:40:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("seja bem vindo");
+function pom() {
+     alert("botão pom clicado");
+}
+document.querySelector('.tecla_pom').onclick = pom;
+
+function clap () { 
+     alert("botão clap clicado ")   
+}
+document.querySelector('.tecla_clap').onclick = clap;
