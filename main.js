0f11ea9b8a88531c9d5a198d79d484f531a22716
escarlet200 <114157080+escarlet200@users.noreply.github.com>
2023-09-25 12:38:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ function pom (){ 
+ document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = pom;
+
+function CLAP (){ 
+    document.querySelector('#som_tecla_CLAP').play();
+   }
+   document.querySelector('.tecla_CLAP').onclick = CLAP;
